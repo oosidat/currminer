f5ce4147470af632d91c6f35d7d7b7d29c59c266
Osama Sidat <oosidat@gmail.com>
2014-10-10 22:04:15 -0400

    parsing data

--- currminer/server/minerserver.js
+++ currminer/server/minerserver.js
@@ -43,7 +43,39 @@ Meteor.methods({
     	var freq = inputjson["frequency"]
     	var path = "{0}{1}-{2}.json".format(dataUrl, curr, freq);
     	console.log(path);
-    	response = Meteor.http.get(path);
-    	console.log(JSON.stringify(response));
+    	Meteor.http.call("GET", path, function(err, result) {
+    		var data = JSON.parse(result.content);
+    		response = parseForCsv(data);
+    		console.log(response);
+    	});
     }
 });
+
+function parseForCsv(data) {
+	allData = [];
+	for(var key in data) {
+		var val = data[key]
+		if (val instanceof Array) {
+			for (var i=0; i < val.length; i++) {
+				var record = val[i];
+				var date = new Date(record[0]);
+				var dollarValue = record[1];
+				csvRecord = {
+					"item"	: key,
+					"date"	: date,
+					"value"	: dollarValue
+				};
+				allData.push(csvRecord);
+			}
+		} else {
+			var date = new Date(val);
+			csvRecord = {
+				"item"	: key,
+				"date"	: date,
+				"value"	: ""
+			};
+			allData.push(csvRecord);
+		}
+	}
+	return allData;
+}
